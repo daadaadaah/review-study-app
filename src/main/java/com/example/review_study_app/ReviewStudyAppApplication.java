77aa23e86168c650600dfbabeb0aa367bd4e76ba
daadaadaah <daadaadaah@gmail.com>
2024-06-23 22:53:24 +0900

    배포 테스트용 로그 추가

--- src/main/java/com/example/review_study_app/ReviewStudyAppApplication.java
+++ src/main/java/com/example/review_study_app/ReviewStudyAppApplication.java
@@ -1,14 +1,17 @@
 package com.example.review_study_app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @EnableScheduling
 @SpringBootApplication
 public class ReviewStudyAppApplication {
 
 	public static void main(String[] args) {
+		log.info("review study scheduler가 시작되었습니다.");
 		SpringApplication.run(ReviewStudyAppApplication.class, args);
 	}
 

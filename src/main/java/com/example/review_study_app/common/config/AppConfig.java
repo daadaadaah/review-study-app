0276ba8466683afa72bd5506ddb4de60252b5dfb
daadaadaah <daadaadaah@gmail.com>
2024-07-07 20:31:33 +0900

    restclient을 bean으로 등록

--- src/main/java/com/example/review_study_app/common/config/AppConfig.java
+++ src/main/java/com/example/review_study_app/common/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.example.review_study_app.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,5 +11,10 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public RestClient.Builder restClient() {
+        return RestClient.builder();
     }
 }

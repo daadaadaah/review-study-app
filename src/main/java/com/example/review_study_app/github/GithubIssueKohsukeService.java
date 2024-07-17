14777dc64366276d0647cd4178e4a96fe8ed465d
daadaadaah <daadaadaah@gmail.com>
2024-07-17 17:05:16 +0900

    refactor: `GithubIssueService` class -> interface

---
+++ src/main/java/com/example/review_study_app/github/GithubIssueKohsukeService.java
@@ -0,0 +1,106 @@
+package com.example.review_study_app.github;
+
+import com.example.review_study_app.reviewstudy.ReviewStudyInfo;
+import java.io.IOException;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHLabel;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class GithubIssueKohsukeService implements GithubIssueService {
+
+    @Value("${github.oauth.accessToken}")
+    private String GITHUB_OAUTH_ACCESS_TOKEN;
+
+    private String REPOSITORY_NAME = ReviewStudyInfo.REPOSITORY_NAME;
+
+    private GitHub github;
+
+    private GHRepository repo;
+
+    private void connectGithub() throws IOException {
+        this.github = GitHub.connectUsingOAuth(GITHUB_OAUTH_ACCESS_TOKEN);
+        this.repo = github.getRepository(REPOSITORY_NAME);
+    }
+
+    // 새로운 주차 Label 생성하는 함수
+    public NewLabelName createNewLabel(int year, int weekNumber) throws IOException {
+        connectGithub();
+
+        String labelName = ReviewStudyInfo.getFormattedThisWeekNumberLabelName(year, weekNumber);
+
+        String labelColor = ReviewStudyInfo.THIS_WEEK_NUMBER_LABEL_COLOR;
+
+        String labelDescription = ReviewStudyInfo.getFormattedThisWeekNumberLabelDescription(year, weekNumber);
+
+        GHLabel ghLabel = repo.createLabel(labelName, labelColor, labelDescription);
+
+        return new NewLabelName(ghLabel.getName());
+    }
+
+    // 라벨 존재 여부 판단 함수
+    public boolean isWeekNumberLabelPresent(String labelName) {
+        try {
+            connectGithub();
+
+            repo.getLabel(labelName);
+            log.info("라벨이 존재합니다. labelName = {}", labelName);
+
+            return true;
+        } catch (Exception exception) {
+            log.warn("라벨이 존재하지 않습니다. exception = {}, labelName = {}", exception.getMessage(), labelName);
+            return false;
+        }
+    }
+
+    // 새로운 주간회고 Issue 생성하는 함수
+    public NewIssue createNewIssue(
+        int currentYear,
+        int currentWeekNumber,
+        String memberFullName,
+        String memberGithubName
+    ) throws IOException {
+        connectGithub();
+
+        String issueTitle = ReviewStudyInfo.getFormattedWeeklyReviewIssueTitle(currentYear, currentWeekNumber, memberFullName);
+
+        String issueBody = ReviewStudyInfo.WEEKLY_REVIEW_ISSUE_BODY_TEMPLATE;
+
+        String thisWeekNumberLabelName = ReviewStudyInfo.getFormattedThisWeekNumberLabelName(currentYear, currentWeekNumber);
+
+        GHIssue ghIssue = repo.createIssue(issueTitle)
+            .assignee(memberGithubName)
+            .label(thisWeekNumberLabelName)
+            .body(issueBody)
+            .create();
+
+        return new NewIssue(ghIssue.getNumber(), ghIssue.getTitle());
+    }
+
+    // Close 할 이슈 목록 가져오는 함수
+    public List<IssueToClose> getIssuesToClose(String labelNameToClose) throws IOException {
+        connectGithub();
+
+        return repo.getIssues(GHIssueState.OPEN)
+            .stream()
+            .filter(ghIssue -> ghIssue.getLabels().stream()
+                .anyMatch(label -> label.getName().equals(labelNameToClose))
+            )
+            .map(ghIssue -> new IssueToClose(ghIssue.getNumber(), ghIssue.getTitle()))
+            .toList();
+    }
+
+    // 이슈 close 하는 함수
+    public void closeIssue(int issueNumber) throws IOException {
+        connectGithub();
+
+        repo.getIssue(issueNumber).close();
+    }
+}
---
+++ src/main/java/com/example/review_study_app/github/NewIssue.java
@@ -0,0 +1,8 @@
+package com.example.review_study_app.github;
+
+public record NewIssue(
+    int number,
+    String title
+) {
+
+}

1f81ba994202610d1b01d4c2a31770a3b65c2767
daadaadaah <daadaadaah@gmail.com>
2024-08-01 13:00:53 +0900

    `DirectSaveLogService` 추가

---
+++ src/main/java/com/example/review_study_app/common/config/AsyncConfig.java
@@ -0,0 +1,21 @@
+package com.example.review_study_app.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Bean(name = "logSaveTaskExecutor")
+    public TaskExecutor logSaveTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1); // 순서 보장하기 위해서 1개
+        executor.setMaxPoolSize(1);
+        executor.setThreadNamePrefix("LogSaveTask-"); // 스레드 이름 접두사
+        executor.initialize();
+        return executor;
+    }
+}

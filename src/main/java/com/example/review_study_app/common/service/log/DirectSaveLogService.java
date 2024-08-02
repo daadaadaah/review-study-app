3dc1591afebfcda5504e08683ed0d9cca3bbbb08
daadaadaah <daadaadaah@gmail.com>
2024-08-02 13:38:39 +0900

    `RestTemplateHttpClientTaskLoggingAspect`에서 비즈니스 로직 모르게 하기

--- src/main/java/com/example/review_study_app/common/service/log/DirectSaveLogService.java
+++ src/main/java/com/example/review_study_app/common/service/log/DirectSaveLogService.java
@@ -4,14 +4,17 @@ import com.example.review_study_app.common.enums.BatchProcessType;
 import com.example.review_study_app.common.repostiory.LogGoogleSheetsRepository;
 import com.example.review_study_app.common.service.log.dto.SaveJobLogDto;
 import com.example.review_study_app.common.service.log.dto.SaveStepLogDto;
+import com.example.review_study_app.common.service.log.dto.SaveTaskLogDto;
 import com.example.review_study_app.common.service.log.entity.ExecutionTimeLog;
 import com.example.review_study_app.common.service.log.entity.GithubApiLog;
 import com.example.review_study_app.common.service.log.entity.JobDetailLog;
 import com.example.review_study_app.common.service.log.entity.StepDetailLog;
+import com.example.review_study_app.task.httpclient.dto.MyHttpResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientResponseException;
 
 
 @Slf4j
@@ -86,9 +89,62 @@ public class DirectSaveLogService implements LogService {
     }
 
     @Async("logSaveTaskExecutor")
-    public void saveTaskLog(GithubApiLog githubApiLog, ExecutionTimeLog executionTimeLog) {
-        logGoogleSheetsRepository.save(githubApiLog);
+    public void saveTaskLog(SaveTaskLogDto saveTaskLogDto) {
 
-        logGoogleSheetsRepository.save(executionTimeLog);
+        long taskDetailLogId = saveTaskLogDto.endTime();
+
+        long timeTaken = saveTaskLogDto.endTime() - saveTaskLogDto.startTime();
+
+        if(saveTaskLogDto.taskResult() instanceof MyHttpResponse) {
+
+            MyHttpResponse myHttpResponse = (MyHttpResponse) saveTaskLogDto.taskResult();
+
+            logGoogleSheetsRepository.save(new GithubApiLog(
+                taskDetailLogId,
+                logHelper.getEnvironment(),
+                saveTaskLogDto.batchProcessName(),
+                saveTaskLogDto.httpMethod(),
+                saveTaskLogDto.myHttpRequest().url(),
+                saveTaskLogDto.myHttpRequest().headers(),
+                saveTaskLogDto.myHttpRequest().body().toString(),
+                myHttpResponse.statusCode(),
+                myHttpResponse.headers(),
+                myHttpResponse.body(),
+                timeTaken,
+                logHelper.getCreatedAt(saveTaskLogDto.endTime())
+            ));
+        } else if(saveTaskLogDto.taskResult() instanceof RestClientResponseException) {
+            RestClientResponseException restClientResponseException = (RestClientResponseException) saveTaskLogDto.taskResult();
+
+            logGoogleSheetsRepository.save(new GithubApiLog(
+                taskDetailLogId,
+                logHelper.getEnvironment(),
+                saveTaskLogDto.batchProcessName(),
+                saveTaskLogDto.httpMethod(),
+                saveTaskLogDto.myHttpRequest().url(),
+                saveTaskLogDto.myHttpRequest().headers(),
+                saveTaskLogDto.myHttpRequest().body().toString(),
+                restClientResponseException.getStatusCode().value(),
+                restClientResponseException.getResponseHeaders(),
+                restClientResponseException.getResponseBodyAsString(),
+                timeTaken,
+                logHelper.getCreatedAt(saveTaskLogDto.endTime())
+            ));
+        } else {
+            // TODO :
+        }
+
+        logGoogleSheetsRepository.save(ExecutionTimeLog.of(
+            logHelper.getTaskId(),
+            logHelper.getStepId(),
+            logHelper.getEnvironment(),
+            BatchProcessType.TASK,
+            saveTaskLogDto.batchProcessName(),
+            saveTaskLogDto.status(),
+            saveTaskLogDto.statusReason(),
+            taskDetailLogId,
+            timeTaken,
+            logHelper.getCreatedAt(saveTaskLogDto.endTime())
+        ));
     }
 }
--- src/main/java/com/example/review_study_app/common/service/log/LogService.java
+++ src/main/java/com/example/review_study_app/common/service/log/LogService.java
@@ -2,9 +2,7 @@ package com.example.review_study_app.common.service.log;
 
 import com.example.review_study_app.common.service.log.dto.SaveJobLogDto;
 import com.example.review_study_app.common.service.log.dto.SaveStepLogDto;
-import com.example.review_study_app.common.service.log.entity.ExecutionTimeLog;
-import com.example.review_study_app.common.service.log.entity.GithubApiLog;
-import com.example.review_study_app.common.service.log.entity.StepDetailLog;
+import com.example.review_study_app.common.service.log.dto.SaveTaskLogDto;
 
 
 public interface LogService {
@@ -13,5 +11,5 @@ public interface LogService {
 
     void saveStepLog(SaveStepLogDto saveStepLogDto);
 
-    void saveTaskLog(GithubApiLog githubApiLog, ExecutionTimeLog executionTimeLog);
+    void saveTaskLog(SaveTaskLogDto saveTaskLogDto);
 }

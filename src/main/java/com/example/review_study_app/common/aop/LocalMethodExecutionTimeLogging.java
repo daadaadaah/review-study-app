4e65e9867f0cf29b000f5f4d7d674ac3306e94dd
daadaadaah <daadaadaah@gmail.com>
2024-08-09 00:30:31 +0900

    `LocalMethodExecutionTimeLogging` 주석 보충

--- src/main/java/com/example/review_study_app/common/aop/LocalMethodExecutionTimeLogging.java
+++ src/main/java/com/example/review_study_app/common/aop/LocalMethodExecutionTimeLogging.java
@@ -8,6 +8,8 @@ import java.lang.annotation.Target;
 
 /**
  * LocalMethodExecutionTimeLogging 은 local 환경에서 특정 메서드의 실행시간을 알고 싶을 때 사용하는 어노테이션이다.
+ *
+ * 기본적으로 실행시간이 log 에 찍히지만, 디스코드 같은 알림 서비스에 보내고 싶으면, isSendNotification 을 활용하면 된다.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)

bfd7e2a12e751c7a1f70dfb04d58b27ce9dcd9e3
daadaadaah <daadaadaah@gmail.com>
2024-08-15 23:20:45 +0900

    `NotificationService`에 ``sendMessageWithFiles` 추가

--- src/main/java/com/example/review_study_app/service/notification/DiscordNotificationService.java
+++ src/main/java/com/example/review_study_app/service/notification/DiscordNotificationService.java
@@ -4,16 +4,23 @@ import static com.example.review_study_app.domain.ReviewStudyInfo.createIssueUrl
 import static com.example.review_study_app.domain.ReviewStudyInfo.createLabelUrl;
 
 import com.example.review_study_app.infrastructure.resttemplate.discord.DiscordRestTemplateHttpClient;
+import com.example.review_study_app.service.notification.dto.UnSavedLogFile;
 import com.example.review_study_app.service.notification.dto.NotificationMessage;
 import com.example.review_study_app.infrastructure.resttemplate.common.dto.MyHttpRequest;
 import com.example.review_study_app.infrastructure.resttemplate.common.dto.MyHttpResponse;
 import com.example.review_study_app.service.github.domain.GithubIssueApiFailureResult;
 import com.example.review_study_app.service.github.domain.GithubIssueApiSuccessResult;
+import com.example.review_study_app.service.notification.factory.file.UnSavedLogFileFactory;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @Slf4j
 @Service
@@ -28,25 +35,81 @@ public class DiscordNotificationService implements NotificationService {
     public static final String EMOJI_CLOCK = ":alarm_clock:";
 
     @Value("${discord.webhook.url}")
-    private String webhookUrl;
+    private String webhookUrl; // TODO : DiscordRestTemplateHttpClient 에 있는게 더 맞을 것 같긴함. 나중에 수정하기
 
-    private final DiscordRestTemplateHttpClient discordGithubRestTemplateHttpClient;
+    private final DiscordRestTemplateHttpClient discordRestTemplateHttpClient;
 
-    public DiscordNotificationService(DiscordRestTemplateHttpClient discordGithubRestTemplateHttpClient) {
-        this.discordGithubRestTemplateHttpClient = discordGithubRestTemplateHttpClient;
+    private final UnSavedLogFileFactory unSavedLogFileFactory;
+
+    public DiscordNotificationService(
+        DiscordRestTemplateHttpClient discordRestTemplateHttpClient,
+        UnSavedLogFileFactory unSavedLogFileFactory
+    ) {
+        this.discordRestTemplateHttpClient = discordRestTemplateHttpClient;
+        this.unSavedLogFileFactory = unSavedLogFileFactory;
     }
 
     @Override
     public boolean sendMessage(String message) {
+
+        discordRestTemplateHttpClient.validateMessage(message);
+
         try {
             HttpHeaders httpHeaders = new HttpHeaders();
             httpHeaders.add("Content-Type", "application/json; utf-8");
 
             MyHttpRequest request = new MyHttpRequest(webhookUrl, httpHeaders, new NotificationMessage(message));
 
-            MyHttpResponse response = discordGithubRestTemplateHttpClient.post(request);
+            MyHttpResponse response = discordRestTemplateHttpClient.post(request);
 
-            if (response.statusCode() != HttpStatus.NO_CONTENT.value()) {
+            int statusCode = response.statusCode();
+
+            if (statusCode != HttpStatus.NO_CONTENT.value() && statusCode != HttpStatus.OK.value()) {
+                log.error("Discord 와의 통신 결과, 다음과 같은 에러가 발생했습니다. HTTPStateCode = {}", response.statusCode());
+                return false;
+            }
+
+            log.info("Discord 로 메시지 전송이 성공했습니다. message = {}", message);
+
+            return true;
+        } catch (Exception exception) {
+            log.error("Discord 로 메시지 전송이 실패했습니다. message = {}, exception = {}", message, exception.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public <T> boolean sendMessageWithFiles(String message, List<UnSavedLogFile> unSavedLogFiles) {
+
+        discordRestTemplateHttpClient.validateMessage(message);
+
+        discordRestTemplateHttpClient.validateFileCount(unSavedLogFiles);
+
+        List<ByteArrayResource> fileResources = unSavedLogFileFactory.generateByteArrayResourcesFromUnSavedLogFile(unSavedLogFiles);
+
+        for(ByteArrayResource fileResource: fileResources) {
+            discordRestTemplateHttpClient.validateFileSize(fileResource); // TODO : 여기서 해주는게 맞나? 밑에서 반복문 1개로 하고, 초과되는 것만 뺴고 전송하는것도 방법일 수 있겠다
+        }
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+            body.add("content", message);
+
+            for (int index = 0; index < fileResources.size(); index++) { // TODO : 총 파일 크기수도 유효성 체크해야 하나?
+                ByteArrayResource fileResource = fileResources.get(index);
+                body.add("file" + index, fileResource);
+            }
+
+            MyHttpRequest request = new MyHttpRequest(webhookUrl, headers, body);
+
+            MyHttpResponse response = discordRestTemplateHttpClient.post(request);
+
+            int statusCode = response.statusCode();
+
+            if (statusCode != HttpStatus.NO_CONTENT.value() && statusCode != HttpStatus.OK.value()) {
                 log.error("Discord 와의 통신 결과, 다음과 같은 에러가 발생했습니다. HTTPStateCode = {}", response.statusCode());
                 return false;
             }
--- src/main/java/com/example/review_study_app/service/notification/NotificationService.java
+++ src/main/java/com/example/review_study_app/service/notification/NotificationService.java
@@ -2,10 +2,14 @@ package com.example.review_study_app.service.notification;
 
 import com.example.review_study_app.service.github.domain.GithubIssueApiFailureResult;
 import com.example.review_study_app.service.github.domain.GithubIssueApiSuccessResult;
+import com.example.review_study_app.service.notification.dto.UnSavedLogFile;
+import java.util.List;
 
 public interface NotificationService { // TODO : 도메인별로 messageFactory 만들어서 리팩토링하면 좋을 것 같음. NotificationService에 지금 너무 책임이 많음
 
     boolean sendMessage(String message);
+
+    <T> boolean sendMessageWithFiles(String message, List<UnSavedLogFile> unSavedLogFiles);
 
     /** 라벨 생성 **/
     String createNewLabelCreationSuccessMessage(String weekNumberLabelName);

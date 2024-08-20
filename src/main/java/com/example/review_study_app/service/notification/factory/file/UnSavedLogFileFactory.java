ff01a3982ba88202792a587bc9e691c83476e3f6
daadaadaah <daadaadaah@gmail.com>
2024-08-20 16:59:41 +0900

    액셀의 셀 최대 크기 초과 이슈 해결
    - 초과된 셀은 json 또는 txt 파일을 하나 더 만들고, 그 파일이름을 해당 셀의 값으로 넣어준다.

--- src/main/java/com/example/review_study_app/service/notification/factory/file/UnSavedLogFileFactory.java
+++ src/main/java/com/example/review_study_app/service/notification/factory/file/UnSavedLogFileFactory.java
@@ -1,7 +1,14 @@
 package com.example.review_study_app.service.notification.factory.file;
 
 import com.example.review_study_app.common.enums.FileType;
+import com.example.review_study_app.common.file.ExcelGenerator;
+import com.example.review_study_app.common.file.JsonGenerator;
+import com.example.review_study_app.common.file.TxtGenerator;
+import com.example.review_study_app.service.notification.vo.FieldToChange;
 import com.example.review_study_app.service.notification.vo.UnSavedLogFile;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,23 +36,86 @@ public class UnSavedLogFileFactory<T> {
         this.txtGenerator = txtGenerator;
     }
 
-    public String createExcelFileNameWithExtension(String fileNameWithoutExtension) {
+    /** UnSavedLogFileFactory 의 역할 1. 확장자를 포함한 파일이름 생성 **/
+    public String createUnSavedLogFileNameWithExtension(String fileNameWithoutExtension) {
         return excelGenerator.createExcelFileNameWithExtension(fileNameWithoutExtension);
     }
 
-    public String createJsonFileNameWithExtension(String fileNameWithoutExtension) {
+    /** UnSavedLogFileFactory 의 역할 2. 액셀 최대 셀 텍스트 크기에 맞게 UnSavedLogFile 변환 **/
+    public  <T> List<UnSavedLogFile> transformUnSavedLogFileByExcelMaxCellTextSize(List<UnSavedLogFile> unSavedLogFiles) {
+        List<UnSavedLogFile> newUnSavedLogFiles = new ArrayList<>();
+
+        List<FieldToChange> fieldToChanges = new ArrayList<>();
+
+        try {
+            for(UnSavedLogFile unSavedLogFile: unSavedLogFiles) {
+
+                T logData = (T) unSavedLogFile.fileData();
+
+                Field[] fields = unSavedLogFile.fileData().getClass().getDeclaredFields();
+
+                for (int i = 0; i < fields.length; i++) {
+                    Field field = fields[i];
+
+                    field.setAccessible(true);
+
+                    String fieldName = field.getName();
+
+                    Object fieldValue = field.get(logData);
+
+                    if(validateUnSavedLog(fieldValue)) {
+
+                        if (isJsonStructurePossible(fieldValue.toString())) {
+                            // JSON 파일
+                            String jsonFileName = createFileNameWithExtensionForObjectOrArray( fieldName + "_" + getFieldValue(logData, "id"));
+
+                            // 해당 필드값을 jsonFileName으로 변경
+                            newUnSavedLogFiles.add(new UnSavedLogFile(jsonFileName, fieldValue));
+
+                            fieldToChanges.add(new FieldToChange(fieldName, jsonFileName));
+
+                        } else {
+                            // txt 파일
+                            String txtFileName = createFileNameWithExtensionForNonObjectOrArray( fieldName + "_" + getFieldValue(logData, "id"));;
+
+                            newUnSavedLogFiles.add(new UnSavedLogFile(txtFileName, fieldValue));
+
+                            fieldToChanges.add(new FieldToChange(fieldName, txtFileName));
+                        }
+                    }
+                }
+
+                String logFileName = unSavedLogFile.fileName();
+
+                if(fieldToChanges.size() > 0) {
+                    T newLogData = createNewInstanceWithModifiedField(logData, fieldToChanges);
+
+                    newUnSavedLogFiles.add(new UnSavedLogFile(logFileName, newLogData));
+
+                } else {
+                    newUnSavedLogFiles.add(new UnSavedLogFile(logFileName, logData));
+                }
+            }
+        } catch (Exception exception) {
+            throw new RuntimeException("createUnSavedLogFiles 에외 발생="+exception.getMessage()); // TODO : 예외 처리 더 고민해보기
+        }
+
+        return newUnSavedLogFiles;
+    }
+
+    private String createFileNameWithExtensionForObjectOrArray(String fileNameWithoutExtension) {
         return jsonGenerator.createJsonFileNameWithExtension(fileNameWithoutExtension);
     }
 
-    public String createTxtFileNameWithExtension(String fileNameWithoutExtension) {
+    private String createFileNameWithExtensionForNonObjectOrArray(String fileNameWithoutExtension) {
         return txtGenerator.createTxtFileNameWithExtension(fileNameWithoutExtension);
     }
 
-    public boolean isExcelCellValueLengthOverLimit(Object value) {
+    private boolean validateUnSavedLog(Object value) {
         return excelGenerator.isExcelValueLengthOverLimit(value);
     }
 
-    public boolean isJsonStructurePossible(String value) {
+    private boolean isJsonStructurePossible(String value) {
         try {
             return jsonGenerator.isJsonStructurePossible(value);
         } catch (Exception exception) {
@@ -53,6 +123,62 @@ public class UnSavedLogFileFactory<T> {
         }
     }
 
+    private Object getFieldValue(Object obj, String fieldName) {
+        try {
+            Field field = obj.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return field.get(obj);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException("Failed to get field value", e);
+        }
+    }
+
+    private <T> T createNewInstanceWithModifiedField(T originalObject, List<FieldToChange> fieldToChanges) throws Exception {
+        if(fieldToChanges == null || fieldToChanges.size() == 0) {
+            // TODO :
+        }
+
+        // 원래 객체의 클래스 정보를 얻습니다.
+        Class<?> clazz = originalObject.getClass();
+
+        // 필드 값을 수정하여 새로운 객체를 생성합니다.
+        Field[] fields = clazz.getDeclaredFields();
+        Object[] fieldValues = new Object[fields.length];
+
+        // 모든 필드를 원본 객체로부터 가져옵니다.
+        for (int i = 0; i < fields.length; i++) {
+            Field field = fields[i];
+            field.setAccessible(true);
+            fieldValues[i] = field.get(originalObject);
+        }
+
+        // 필드 값을 수정합니다.
+        for (FieldToChange fieldToChange : fieldToChanges) {
+            String fieldName = fieldToChange.fieldNameToChange();
+            Object newValue = fieldToChange.dataFileName();
+
+            boolean fieldModified = false;
+            for (int i = 0; i < fields.length; i++) {
+                Field field = fields[i];
+                if (field.getName().equals(fieldName)) {
+                    fieldValues[i] = newValue;
+                    fieldModified = true;
+                    break;
+                }
+            }
+
+            if (!fieldModified) {
+                throw new NoSuchFieldException("Field " + fieldName + " does not exist in class " + clazz.getName());
+            }
+        }
+
+        // 생성자 정보를 얻습니다.
+        Constructor<?> constructor = clazz.getConstructors()[0];
+
+        return (T) constructor.newInstance(fieldValues);
+    }
+
+    /** UnSavedLogFileFactory 의 역할 3. UnSavedLogFile 목록을 토대로 ByteArrayResources 생성 **/
     public List<ByteArrayResource> generateByteArrayResourcesFromUnSavedLogFile(List<UnSavedLogFile> files) {
         return files.stream()
             .map(file -> createByteArrayResourceFromFileData(file.fileName(), (T) file.fileData()))
